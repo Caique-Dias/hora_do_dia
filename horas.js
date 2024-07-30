aa92e4f218705324ffd465b439181449360ba658
Caique R. <caiquecruz.dabahia@hotmail.com>
2024-07-30 17:57:48 -0300

    Initial commit

---
+++ horas.js
@@ -0,0 +1,38 @@
+let img = document.querySelector('#img_horas')
+let txt = document.querySelector('#horas')
+
+let date = new Date
+let horas = date.getHours()
+
+let momento 
+
+if (horas > 4 && horas < 12){
+    img.src = "imgs/manha-pq.jpg"
+    document.body.style.backgroundColor = "#e2cd9f"
+    momento = "manhã"
+    console.log("Manha")
+}
+
+else if(horas >= 12 && horas < 18){
+    img.src = "imgs/tarde-pq.png"
+    document.body.style.backgroundColor = "#5299CD"
+    momento = "tarde"
+    console.log("Tarde")
+}
+
+else if(horas >= 18){
+    momento = "noite"
+    img.src = "imgs/noite-pq.png"
+    document.body.style.backgroundColor = "#515154"
+
+}
+
+else{
+    momento = "madrugada"
+    img.src = "imgs/noite-pq.png"
+    document.body.style.backgroundColor = "#515154"
+}
+
+
+txt.innerHTML = `Agora são <strong>${horas} </strong>da <strong>${momento}</strong>.`
+
